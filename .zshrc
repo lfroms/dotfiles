b350a8a87d278b4e2784e9f407059dd001a68982
Lukas Romsicki <lukas.romsicki@shopify.com>
2021-03-25 15:49:19 -0400

    Add ZSH config and setup script

---
+++ .zshrc
@@ -0,0 +1,11 @@
+export ZSH="$HOME/.oh-my-zsh"
+
+ZSH_THEME="spaceship"
+
+plugins=(
+  git 
+  zsh-autosuggestions 
+  zsh-syntax-highlighting
+)
+
+source $ZSH/oh-my-zsh.sh
---
+++ setup.sh
@@ -0,0 +1,18 @@
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Install Oh My Zsh plugins
+git clone https://github.com/denysdovhan/spaceship-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt --depth=1
+ln -sf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship.zsh-theme
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Remove old configuration
+mv ~/.zshrc ~/.zshrc.old
+
+# Link Zsh config
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# Reload zsh config
+source ~/.zshrc
